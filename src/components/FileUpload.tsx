36481a1c7ac61daf273dbf3d4e856e986f37f90d
jwcfrey <bondanbanuaji@gmail.com>
2025-01-30 20:15:39 +0700

    update aws

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,10 +1,31 @@
-'use client'
-import React from 'react'
-import { useDropzone } from 'react-dropzone'
-import { Inbox } from 'lucide-react'
+'use client';
+
+import { Inbox } from 'lucide-react';
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
 
 const FileUpload = () => {
-    const { getRootProps, getInputProps } = useDropzone()
+    const { getRootProps, getInputProps } = useDropzone({
+        accept:{ 'application/pdf': [".pdf"] },
+        maxFiles: 1,
+        onDrop: async (acceptedFiles) => {
+            console.log(acceptedFiles);
+            const file = acceptedFiles[0]
+            if (file.size > 10 * 1024 * 1024) {
+                //bigger than 10mb
+                alert('please upoad a smaller file')
+                return
+            }
+
+            try {
+                const data = await uploadToS3(file);
+                console.log('data', data);
+            } catch (error) {
+                console.log(error)
+            }
+            
+        },
+    });
     return (
         <div className="p-2 bg-white rounded-xl">
             <div
@@ -13,9 +34,8 @@ const FileUpload = () => {
                 })}
             >
                 <input {...getRootProps} />
-                <Inbox className="w-10">
-                    
-                </Inbox>
+                <Inbox className="w-10 h-10 text-blue-500" />
+                <p className="mt-2 text-sm text-slate-400">Taruh File PDF Disini</p>
             </div>
         </div>
     );
